436f50da23518f27be2d0657796ec377bafac358
IaroslavtsevVIu <iaroslavtsevviu@alfastrah.ru>
2021-05-26 14:36:59 +0300

    Базовая структура проекта

---
+++ src/components/app/app.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from '../../images/logo.svg';
+import styles from './app.module.css';
+
+function App() {
+  return (
+    <div className={styles.app}>
+      <header className={styles.header}>
+        <img src={logo} className={styles.logo} alt="logo" />
+        <p>
+          Редактируйте <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className={styles.link}
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
