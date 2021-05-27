5661c9ce9715af41bde9b905a8a81341fc098ffc
Владимир Ярославцев <vl.yaroslavtsev@gmail.com>
2021-05-28 00:10:48 +0300

    Удаление .tsx

--- src/components/app-header/app-header.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import styles from "./app.module.css";
-import { Tab } from "@ya.praktikum/react-developer-burger-ui-components";
-
-function AppHeader() {
-  return <header className={styles.header}></header>;
-}
-
-export default AppHeader;
--- src/components/burger-constructor/burger-constructor.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import styles from "./burger-ingredients.module.css";
-import { Tab } from "@ya.praktikum/react-developer-burger-ui-components";
-
-function BurgerConstructor() {
-  return <section className={styles.container}></section>;
-}
-
-export default BurgerConstructor;
