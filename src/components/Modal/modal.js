059354a12864274b7c8247dd2b3277c25be8c1b1
Владимир Ярославцев <vl.yaroslavtsev@gmail.com>
2021-06-03 00:40:59 +0300

    заготовка для Modal и ModalOverlay

---
+++ src/components/Modal/modal.js
@@ -0,0 +1,8 @@
+import { useState, useEffect } from "react";
+import styles from "./modal.module.css";
+
+function Modal() {
+  return <></>;
+}
+
+export default Modal;
---
+++ src/components/ModalOverlay/modal-overlay.js
@@ -0,0 +1,8 @@
+import { useState, useEffect } from "react";
+import styles from "./modal-overlay.module.css";
+
+function ModalOverlay() {
+  return <></>;
+}
+
+export default ModalOverlay;
