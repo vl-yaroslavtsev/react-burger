8665faefd508ea77b38cf8ec32d2820fe604a167
Владимир Ярославцев <vl.yaroslavtsev@gmail.com>
2021-08-02 19:02:35 +0300

    Сквозные тесты доступности страниц для зарегистрированного и незарегистированного пользователя

---
+++ cypress/integration/pages-available.spec.js
@@ -0,0 +1,78 @@
+import order from "../fixtures/order.json";
+import ingredient from "../fixtures/ingredient.json";
+
+describe("Доступность страниц для незарегистированного пользователя", () => {
+  it("Главная страница, на которой грузятся ингредиенты", () => {
+    cy.visit("/");
+    cy.contains("Соберите бургер");
+    cy.contains(ingredient.name);
+    cy.contains(ingredient.price);
+  });
+
+  it("Детальная страница ингредиента с данными", () => {
+    cy.visit(`/ingredients/${ingredient.id}`);
+    cy.contains("Детали ингредиента");
+    cy.contains(ingredient.name);
+  });
+
+  it("Лента заказов", () => {
+    cy.visit("/feed", { timeout: 7000 });
+    cy.contains("Готовы:");
+    cy.contains("В работе:");
+    cy.contains("Выполнено за все время:");
+    cy.contains("Выполнено за сегодня:");
+  });
+
+  it("Детальная страница заказа с данными", () => {
+    cy.visit(`/feed/${order.id}`);
+    cy.contains(order.id);
+    cy.contains(order.name);
+    cy.contains(order.price);
+  });
+
+  it("Страница входа", () => {
+    cy.visit("/login");
+    cy.contains("Вход");
+  });
+
+  it("Страница регистрации", () => {
+    cy.visit("/register");
+    cy.contains("Регистрация");
+  });
+
+  it("Страница восстановления пароля", () => {
+    cy.visit("/forgot-password");
+    cy.contains("Восстановление пароля");
+  });
+
+  it("Страница сброса пароля. Редирект на восстановление пароля", () => {
+    cy.visit("/reset-password");
+    cy.url().should("include", "/forgot-password");
+  });
+
+  it("Страница профиля. Редирект на вход", () => {
+    cy.visit("/profile");
+    cy.url().should("include", "/login");
+  });
+
+  it("Страница заказов в профиле. Редирект на вход", () => {
+    cy.visit("/profile/orders/");
+    cy.url().should("include", "/login");
+  });
+
+  it("Детальная страница заказа в профиле. Редирект на вход", () => {
+    cy.visit("/profile/orders/1308");
+    cy.url().should("include", "/login");
+  });
+
+  it("Выход из профиля. Редирект на вход", () => {
+    cy.visit("/profile/logout");
+    cy.url().should("include", "/login");
+  });
+
+  it("Несуществующая страница", () => {
+    cy.visit("/asdfsdfasdfdsaf");
+    cy.contains("404");
+    cy.contains("Упс!");
+  });
+});
--- src/services/utils.js
+++ src/services/utils.js
@@ -1,5 +1,4 @@
-import { useEffect, useLayoutEffect, useRef } from "react";
-import appStyles from "../components/app/app.module.css";
+import { useEffect, useRef } from "react";
 
 export function getCookie(name) {
   const matches = document.cookie.match(
@@ -157,48 +156,6 @@ export function animate({ timing = "easeInOut", draw, duration }) {
       }
     });
   });
-}
-
-export function useScrollbar(
-  ref,
-  { exclude = [], props = [], maxHeight, isInModal } = {}
-) {
-  if (!Array.isArray(exclude)) {
-    exclude = [exclude];
-  }
-  useLayoutEffect(() => {
-    const el = ref.current;
-
-    if (!el) return;
-
-    const bottom = exclude.reduce((acc, ref) => {
-      const el = ref.current;
-      if (!el) return acc;
-      const style = getComputedStyle(el);
-
-      return (
-        acc +
-        el.offsetHeight +
-        parseInt(style.marginTop) +
-        parseInt(style.marginBottom)
-      );
-    }, 0);
-    const modalFix = isInModal ? "(5vh + 41px)" : "0px";
-    const { top } = el.getBoundingClientRect();
-    const aroundSpace = `(${modalFix} + ${Math.ceil(top + bottom)}px)`;
-    if (maxHeight) {
-      el.style.maxHeight = `calc(min(100vh - ${aroundSpace}, 
-      ${maxHeight}px))`;
-    } else {
-      el.style.maxHeight = `calc(100vh - ${aroundSpace})`;
-    }
-    el.classList.add(appStyles.customScrollbar);
-
-    return () => {
-      el.classList.remove(appStyles.customScrollbar);
-      el.style.maxHeight = "";
-    };
-  }, [ref, maxHeight, isInModal, exclude, ...props]);
 }
 
 // Хук: получаем предыдущее значение пропса или состояния
