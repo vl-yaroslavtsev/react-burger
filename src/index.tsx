543a08d76df2f8a0ad5268c042966ade882a9f1d
IaroslavtsevVIu <iaroslavtsevviu@alfastrah.ru>
2021-08-10 15:55:49 +0300

    доработка рутинга для gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ import { Provider } from "react-redux";
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router>
+      <Router basename="/react-burger">
         <App />
       </Router>
     </Provider>
