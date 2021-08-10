545d26be019b660ad701663c712fbc129d660dc9
IaroslavtsevVIu <iaroslavtsevviu@alfastrah.ru>
2021-08-10 13:13:34 +0300

    очистка ветки

--- src/services/hooks.ts
+++
@@ -1,9 +0,0 @@
-import {
-  TypedUseSelectorHook,
-  useDispatch as dispatchHook,
-  useSelector as selectorHook,
-} from "react-redux";
-import { AppDispatch, RootState, AppThunk } from "./types";
-
-export const useDispatch = () => dispatchHook<AppDispatch | AppThunk>();
-export const useSelector: TypedUseSelectorHook<RootState> = selectorHook;
